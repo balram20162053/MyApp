0d3b79d4caaabb6c8c5b7b75f9ab0a0a410d34d5
BACHAUDH <balramchaudhary10225@gmail.com>
2020-10-15 15:07:54 +0530

    updated helloworld java

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -5,7 +5,17 @@ public class HelloWorld {
 		System.out.println("hello world ,this is from github.");
 		
 		Customer c1=new Customer("Balram Chaudhary","India");
+		Customer c2=new Customer("Aditya kumar","India");
+		Customer c3=new Customer("Gaurav sayana","India");
+		Customer c4=new Customer("Ravi Kumar","India");
+		Customer c5=new Customer("Satish lok..","India");
+		
 		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		System.out.println(c5);
+		
 	}
 
 }
