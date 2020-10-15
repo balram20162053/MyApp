85dc76576c1536ce9a3df7ea71b178dcd6e339a8
BACHAUDH <balramchaudhary10225@gmail.com>
2020-10-15 14:58:05 +0530

    second commit or version

--- MyApp/src/main/java/com/ofss/HelloWorld.java
+++ MyApp/src/main/java/com/ofss/HelloWorld.java
@@ -3,6 +3,9 @@ package com.ofss;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world ,this is from github.");
+		
+		Customer c1=new Customer("Balram Chaudhary","India");
+		System.out.println(c1);
 	}
 
 }
